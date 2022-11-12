2c0251667853a17147365aeed3c9218ff5d62a64
pitabyte <78605465+pitabyte@users.noreply.github.com>
2022-11-12 22:45:42 +0100

    Add files via upload

---
+++ HexCodeTask/mysite/hexCodeTask/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render
+from django.urls import reverse
+from rest_framework.response import Response
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from .models import Photo, CustomUser, Thumbnail, BinaryPhoto
+from .serializers import PhotoSerializer, ThumbnailSerializer, BinaryPhotoSerializer
+from PIL import Image, ImageOps
+import io
+
+# Create your views here.
+
+@api_view(['GET'])
+def index(request):
+    return Response("Hello world")
+
+@api_view(['GET'])
+def getPhotos(request):
+    data = request.data
+    username = data['username']
+    password = data['password']
+    user = CustomUser.objects.get(username=username, password=password)
+    photos = Photo.objects.filter(user=user)
+    serializer = PhotoSerializer(photos, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getPhoto(request, id):
+    if Photo.objects.filter(id=id).exists():
+        photo = Photo.objects.get(id=id)
+        serializer = PhotoSerializer(photo)
+        return Response(serializer.data)
+    else:
+        content = {"This photo doesn't exist"}
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['POST'])
+def uploadPhoto(request):
+    data = request.data
+    #photo_data = data['photo']
+    username = data['username']
+    password = data['password']
+    user = CustomUser.objects.get(username=username, password=password)
+    photo_data = request.FILES.get('photo')
+    photo = Photo(user=user, image=photo_data)
+    photo.save()
+
+    heights = user.tier.get_heights_list()
+    baseURL = request.build_absolute_uri(reverse('index'))
+    returnList = []
+    for height in heights:
+        thumbnail = Thumbnail(user=user, photo=photo, height=height)
+        thumbnail.save()
+        finalURL = baseURL + "thumbnail/" + str(thumbnail.id)
+        thumbnail.url = finalURL
+        thumbnail.save()
+        returnList.append(ThumbnailSerializer(thumbnail).data)
+    if (user.tier.gets_original == True):
+        returnList.append(PhotoSerializer(photo).data)
+    if (user.tier.expiring_link == True and data['expires'] != None):
+        binaryPhoto = BinaryPhoto(user=user, photo=photo, expires=data['expires'])
+        binaryPhoto.save()
+        finalURL = baseURL + "binary/" + str(binaryPhoto.id)
+        binaryPhoto.url = finalURL
+        binaryPhoto.save()
+        returnList.append(BinaryPhotoSerializer(binaryPhoto).data)
+    return Response(returnList)
+    return Response('photo was uploaded')
+
+@api_view(['GET'])
+def getThumbnail(request, id):
+    thumbnail = Thumbnail.objects.get(id=id)
+    image = Image.open(thumbnail.photo.image.path)
+    ratio = image.width / image.height
+    MAX_SIZE = (thumbnail.height * ratio, thumbnail.height)
+    image.thumbnail(MAX_SIZE)
+    output = io.BytesIO()
+    image.save(output, format='JPEG')
+    print(image.width)
+    print(image.height)
+    hex_data = output.getvalue()
+    return HttpResponse(hex_data, content_type="image/jpeg")
+
+@api_view(['GET'])
+def getBinaryPhoto(request, id):
+    binaryPhoto = BinaryPhoto.objects.get(id=id)
+    image = Image.open(binaryPhoto.photo.image.path)
+    grayImage = ImageOps.grayscale(image)
+    output = io.BytesIO()
+    grayImage.save(output, format='JPEG')
+    hex_data = output.getvalue()
+    return HttpResponse(hex_data, content_type="image/jpeg")
