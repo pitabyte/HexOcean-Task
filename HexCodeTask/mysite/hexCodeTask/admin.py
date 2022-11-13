817bd13d5423707460d2c652f13dbeabe654f3dd
pitabyte <78605465+pitabyte@users.noreply.github.com>
2022-11-13 11:31:12 +0100

    Delete HexCodeTask/mysite directory

--- HexCodeTask/mysite/hexCodeTask/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import CustomUser, Photo, Tier
-# Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(Photo)
-admin.site.register(Tier)
